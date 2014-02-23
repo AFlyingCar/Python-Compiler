262af140a1f2cf52b27195c1fc6db38fcafc9af9
Tyler Robbins <tyler@familyrobbins.com>
2014-02-22 17:45:13 -0800

    Removed broken method of copying root files. Added note and debugging messages. Added packaging messages.

--- py-pydTEST.py
+++ py-pydTEST.py
@@ -12,27 +12,37 @@ if not os.path.exists(os.getcwd() + "\\bin"): #generate \\bin file
 	os.mkdir(os.getcwd() + "\\bin")
 	print "Generating \\bin"
 
-for item in os.listdir("C:\\Python27\\libs"):
-	include[item] = "C:\\Python27\\libs"
-
-#for item in os.listdir(os.getcwd() + "\\src"):
 finder.run_script("HelloWorld.py")
 
 os.chdir("bin")
 
-print "Loaded modules: "
+if not os.path.exists(os.getcwd() + "\\Lib"):
+	os.mkdir(os.getcwd() + "\\Lib")
+	print "Generating \\bin\\Lib"
+
+if not os.path.exists(os.getcwd() + "\\libs"):
+	os.mkdir(os.getcwd() + "\\libs")
+	print "Generating \\bin\\libs"
+
+for root, dirs, files in os.walk("C:\\Python27\\libs"):
+	for file in files:
+		print "Copying " + file + " "*4 + "-"*5 + " "*4 + os.getcwd() + "\\libs"
+		os.system("copy " + root + "\\" + file + " " + os.getcwd() + "\\libs")
+
+print "\nIndexing loaded modules: \n"
 for name, mod in finder.modules.iteritems():
 	print '%s: ' % name,
 	print ','.join(mod.globalnames.keys()[:3])
 	modules.append(name)
 
 print '-'*50
-print "Modules not imported: "
-print '\n'.join(finder.badmodules.iterkeys())
+#print "\nModules not imported: "
+#print '\n'.join(finder.badmodules.iterkeys())
 
-print "\n\n"
+print "\nLoaded modules: \n[",
 for item in modules:
-	print item
+	print item + ", ",
+print "]"
 
 for item in modules:
 	for root, dirs, files in os.walk("C:\\Python27\\Lib"):
@@ -41,33 +51,20 @@ for item in modules:
 				print file
 				include[file] = root
 
+print '-'*100
+
+root = os.getcwd() + "\\Lib"
+
+#If it turns out that the Python root files need to be in the same file structure, copy the directory tree in '\Lib'
+#instead of copying each folder separately and placing each file in it's respective folder separately.
+
+print "Copying Python modules"
 for item in include:
-	print "copy " + item + " " + os.getcwd() + "\\bin"
-	if include[item].endswith("\\Lib"):
-		if not os.path.exists(os.getcwd() + "\\Lib"):
-			os.mkdir(os.getcwd() + "\\Lib")
-		os.system("copy " + include[item] + "\\" + item + " " + os.getcwd() + "\\bin" + "\\Lib")
-	
-	else:
-		path = include[item][include[item].index("\\Lib") + len("\\Lib"):]
-		folder = path.split("\\")[len(path.split("\\")) - 1]
+	print "copying " + item
+	print "Origin: " + include[item]
+	os.system("copy " + include[item] + "\\" + item + " " + root)
+	print "New location: " + root
 
-		for item in path.split("\\"):
-#			if item not in os.path.exists(os.getcwd() + path):
-			if not os.path.exists(os.getcwd() + path[path.index("\\" + item):]):
-				print path[path.index("\\" + item):]
-				dirs = os.getcwd()
-				for item in path[path.index("\\" + item):].split("\\"):
-					if item == "":
-						continue
-					os.mkdir(item)
-					os.chdir(item)
-#					dirs = dirs + "\\" + item
-#					print dirs
-				os.chdir(dirs)
-
-		os.system("copy " + include[item] + "\\" + item + " " + os.getcwd() + path)
-
-print include
+print "Finished copying loaded modules into " + root
 
 nuclear = u'\u2622'
